2b2dab31d1f707288ba952c38167bd51e1ab8ba0
Kumar Aditya <ka09934147022@gmail.com>
2020-07-30 20:48:06 +0530

    Adding the instagram card View

--- App.js
+++ App.js
@@ -1,32 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import {Image, View } from 'react-native';
+import {Image,StyleSheet, View } from 'react-native';
+import { AppLoading } from "expo";
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
 import CardImage from './components/CardImage'
+import FooterBadge from './components/FooterBadge'
 
-export default class App extends Component  {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
 
-  async componentWillMount() {
-      await Expo.Font.loadAsync({
-        'Roboto': require('native-base/Fonts/Roboto.ttf'),
-        'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-      });
+  async componentDidMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
     }
-    render(){
-  return (
 
-    <CardImage></CardImage>
-
-
-  );
+    return (
+      <View style = {styles.container}>
+      <CardImage/>
+      <FooterBadge/>
+      </View>
+    );
+  }
 }
-}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: '#fff'
+  },
+});
--- components/CardImage.js
+++ components/CardImage.js
@@ -8,7 +8,6 @@ export default class CardImage extends Component {
   render() {
     return (
       <Container>
-          <Header />
           <Content>
             <Card>
               <CardItem>
